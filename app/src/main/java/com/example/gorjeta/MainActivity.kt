4c6cc2625341b1d3f0ffd3f60744189d1a1ad1d3
mairalima <maijucelima@gmail.com>
2025-01-13 10:59:01 -0300

    App Layout Construction for TipsCalculator

---
+++ app/src/main/java/com/example/gorjeta/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.gorjeta
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // valor total da conta
+    // numero de pessoas
+    //porcentagem da gorjeta
+    //10%, 15%  ou 20%
+    //Calcular
+    //Limpar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
