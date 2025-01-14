decc6e9b070213bf3c46b0fd3073c730abe3c437
mairalima <maijucelima@gmail.com>
2025-01-14 11:24:58 -0300

    Utilizando View binding para recuperar campos

--- app/src/main/java/com/example/gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/gorjeta/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.gorjeta
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.CompoundButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +32,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rdOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            println("Maira: $isChecked")
+
+        }
+
+        binding.rdOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            println("Maira: $isChecked")
+
+        }
+
+        binding.rdOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            println("Maira: $isChecked")
+
+        }
+
 
         binding.btnClean.setOnClickListener {
             println("Maira " + binding.tieTotal.text)
